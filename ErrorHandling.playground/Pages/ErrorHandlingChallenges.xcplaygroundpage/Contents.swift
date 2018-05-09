0068b4a57ab0a41141219332dd30ecab41c808c7
tdimnet <thomas.dimnet@gmail.com>
2018-05-09 13:13:19 +0200

    Add error handling

---
+++ ErrorHandling.playground/Pages/ErrorHandlingChallenges.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+// 1.1
+/*
+ 1.1
+    For this task, start by creating an enum to model a series of errors. Name the error type NetworkError.
+ 1.2
+    Add a single enum member, invalidURL to the error object we created.
+ */
+
+enum NetworkError: Error {
+   case invalidURL
+}
