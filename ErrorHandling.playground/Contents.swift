f60208edca70d9ea4ee71a389efe1142f04fc70d
tdimnet <thomas.dimnet@gmail.com>
2018-05-09 13:09:02 +0200

    Add basic code and start working with error

--- ErrorHandling.playground/Contents.swift
+++ ErrorHandling.playground/Contents.swift
@@ -1,1 +1,17 @@
+struct Friend {
+    let name: String
+    let age: String
+    let address: String?
+}
 
+enum FriendError: Error {
+    case invalidData
+}
+
+func friend(from dict: [String: String]) -> Friend {
+    guard let name = dict["name"], let age = dict["age"] else { return nil }
+    
+    let address = dict["address"]
+    
+    return Friend(name: name, age: age, address: address)
+}
