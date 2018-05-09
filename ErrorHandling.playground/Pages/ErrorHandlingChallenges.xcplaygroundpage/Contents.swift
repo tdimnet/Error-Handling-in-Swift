3530ceb2dd3e6fc858db54deb1bf59203425ab21
tdimnet <thomas.dimnet@gmail.com>
2018-05-09 13:34:54 +0200

    Add second error challenge

--- ErrorHandling.playground/Pages/ErrorHandlingChallenges.xcplaygroundpage/Contents.swift
+++ ErrorHandling.playground/Pages/ErrorHandlingChallenges.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-// 1.1
+// 1
 /*
  1.1
     For this task, start by creating an enum to model a series of errors. Name the error type NetworkError.
@@ -8,4 +8,19 @@
 
 enum NetworkError: Error {
    case invalidURL
+}
+
+
+
+// 2
+/*
+ For this task, let's get some practice throwing an error. You've been provided with an error object in the editor. Errors are typically thrown from inside functions so let's start by creating a function that can throw.
+ Name this function lend. It takes no parameters and has no return type.
+ */
+enum BookError: Error {
+    case incorrectTitle
+}
+
+func lend() throws {
+    throw BookError.incorrectTitle
 }
